d21c1adeeca233b915a4cf32bd52845b4c7acd2b
zelloss <zelloss@gmail.com>
2025-06-09 23:28:11 +0200

    slightly bigger model

--- chat.py
+++ chat.py
@@ -4,13 +4,16 @@ from langchain.prompts import (
     PromptTemplate,
     SystemMessagePromptTemplate,
 )
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_chroma import Chroma
 from langchain.schema.runnable import RunnablePassthrough
+from langchain_huggingface import HuggingFaceEmbeddings
+
 import dotenv
 dotenv.load_dotenv()
 FAQ_CHROMA_PATH="chroma_data"
+MODEL_NAME="./models/all-mpnet-base-v2"
 
 # First define the system & human prompts -> chat prompt template
 roboasistant_system_template_str = """You are a member of the robotic team. 
@@ -43,9 +46,14 @@ chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
 
 output_parser = StrOutputParser()
 
+embeddings  = HuggingFaceEmbeddings(
+    model_name=MODEL_NAME, #local path
+    model_kwargs={'device': 'cpu'}  # or 'cuda' if you have GPU 
+)
+
 faqs_vector_db = Chroma(
     persist_directory=FAQ_CHROMA_PATH,
-    embedding_function=OpenAIEmbeddings(),
+    embedding_function=embeddings,
 )
 
 faqs_retriever = faqs_vector_db.as_retriever(k=3)
--- vector_loader.py
+++ vector_loader.py
@@ -1,10 +1,14 @@
-from langchain.text_splitter import MarkdownTextSplitter, RecursiveCharacterTextSplitter
+from langchain.text_splitter import MarkdownTextSplitter
 from langchain.schema import Document
 from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
 import dotenv
 dotenv.load_dotenv()
 FAQ_CHROMA_PATH="chroma_data"
+# MODEL_NAME="sentence-transformers/all-MiniLM-L6-v2"
+# MODEL_NAME="./models/all-MiniLM-L6-v2"
+MODEL_NAME="./models/all-mpnet-base-v2"
 
 #load myiam
 with open("documents/myiam.txt", "r") as f:
@@ -34,8 +38,15 @@ for chunk in chunks:
     documents.append(Document(page_content=chunk, metadata={"header": header, "category": "PO"}))
 
 
-
 # Generate embeddings and store them on disk
-embeddings = OpenAIEmbeddings()
-# vector_store = Chroma.from_texts(chunks, embeddings)
-vector_store = Chroma.from_documents(documents, embeddings, persist_directory=FAQ_CHROMA_PATH)
+embeddings = HuggingFaceEmbeddings(
+    model_name=MODEL_NAME, #local path
+    model_kwargs={'device': 'cpu'}  # or 'cuda' if you have GPU 
+)
+
+# Create vector store
+vectorstore = Chroma.from_documents(
+    documents=documents,
+    embedding=embeddings,
+    persist_directory=FAQ_CHROMA_PATH
+)
